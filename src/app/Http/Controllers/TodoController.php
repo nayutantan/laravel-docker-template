be289ce21e98f21924cc38eb31382c1f13fefa35
nayutantan <rorushinamon5@gmail.com>
2026-01-13 14:06:33 +0900

    add: バリデーションの実装

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\TodoRequest;
 use App\Todo;
 
 class TodoController extends Controller
@@ -24,10 +24,9 @@ class TodoController extends Controller
     {
         return view('todo.create');
     }
-    public function store(Request $request)
+    public function store(TodoRequest $request)
     {
-            $inputs = $request->all();
-
+        $inputs = $request->all();
         $this->todo->fill($inputs);
         $this->todo->save();
 
@@ -44,11 +43,12 @@ class TodoController extends Controller
         $todo = Todo::findOrFail($id);
         return view('todo.edit', ['todo' => $todo]);
     }
-    public function update(Request $request, $id)
+    public function update(TodoRequest $request, $id)
     {
         $inputs = $request->all();
-        $todo = Todo::findOrFail($id);
-        $todo->fill($inputs)->save();
+        $todo = $this->todo->find($id);
+        $todo->fill($inputs);
+        $todo->save();
 
         return redirect()->route('todo.show', $todo->id);
     }
